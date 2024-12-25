8565e3cdcb322d6f3a0bdfb553284607ce2193b3
migueraoui <mohamedilyesgueraoui@gmail.com>
2024-12-25 16:25:19 +0100

    add GarageDoor.java file

---
+++ model/GarageDoor.java
@@ -0,0 +1,47 @@
+package sp.migr.garagedoorcontrol.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+public class GarageDoor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String status;
+    private LocalDateTime lastUpdated;
+
+    public GarageDoor() {
+        this.status = "closed";
+        this.lastUpdated = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+        this.lastUpdated = LocalDateTime.now();
+    }
+
+    public LocalDateTime getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(LocalDateTime lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+}
